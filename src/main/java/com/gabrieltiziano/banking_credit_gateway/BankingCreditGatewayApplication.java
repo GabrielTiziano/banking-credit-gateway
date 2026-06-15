fe9daa469e4b1f4582c1cee3c04fb0d59253ecd0
GGhiaroni <gghiaronitiziano@gmail.com>
2026-06-15 10:23:44 -0300

    first commmit

---
+++ src/main/java/com/gabrieltiziano/banking_credit_gateway/BankingCreditGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.gabrieltiziano.banking_credit_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingCreditGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingCreditGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrieltiziano/banking_credit_gateway/BankingCreditGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrieltiziano.banking_credit_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingCreditGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
