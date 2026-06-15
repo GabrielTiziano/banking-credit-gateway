85b5785f28d3041471229f9e165c00a0d9290e1d
GGhiaroni <gghiaronitiziano@gmail.com>
2026-06-15 14:21:23 -0300

    feat: Cria os esqueletos iniciais dos Controllers de Usuario e Proposta

---
+++ src/main/java/com/gabrieltiziano/banking_credit_gateway/controller/PropostaController.java
@@ -0,0 +1,17 @@
+package com.gabrieltiziano.banking_credit_gateway.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/proposta")
+public class PropostaController {
+    @PostMapping
+    public ResponseEntity criar(@RequestBody PropostaRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body();
+    }
+}
---
+++ src/main/java/com/gabrieltiziano/banking_credit_gateway/controller/UsuarioController.java
@@ -0,0 +1,9 @@
+package com.gabrieltiziano.banking_credit_gateway.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class UsuarioController {
+}
