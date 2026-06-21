be249398c4461925a9646f2b3a4f49f63e873506
GGhiaroni <gghiaronitiziano@gmail.com>
2026-06-21 15:19:39 -0300

    feat: Adiciona o nome da exchange no application.yaml

--- src/main/java/com/gabrieltiziano/banking_credit_gateway/service/PropostaService.java
+++ src/main/java/com/gabrieltiziano/banking_credit_gateway/service/PropostaService.java
@@ -5,6 +5,7 @@ import com.gabrieltiziano.banking_credit_gateway.dto.PropostaResponse;
 import com.gabrieltiziano.banking_credit_gateway.entities.Proposta;
 import com.gabrieltiziano.banking_credit_gateway.mapper.PropostaMapper;
 import com.gabrieltiziano.banking_credit_gateway.repository.PropostaRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,16 +16,21 @@ public class PropostaService {
     private final PropostaRepository propostaRepository;
     private final NotificacaoService notificacaoService;
 
-    public PropostaService(PropostaRepository propostaRepository, NotificacaoService notificacaoService) {
+    private final String exchange;
+
+    public PropostaService(PropostaRepository propostaRepository,
+                           NotificacaoService notificacaoService,
+                           @Value("${rabbitmq.propostapendente.exchange}") String exchange) {
         this.propostaRepository = propostaRepository;
         this.notificacaoService = notificacaoService;
+        this.exchange = exchange;
     }
 
     @Transactional
     public PropostaResponse criar(PropostaRequest request) {
         Proposta propostaSalva = propostaRepository.save(PropostaMapper.toProposta(request));
         PropostaResponse dto = PropostaResponse.from(propostaSalva);
-        notificacaoService.notificar(dto, "proposta-pendente.ex");
+        notificacaoService.notificar(dto, exchange);
         return dto;
     }
 
