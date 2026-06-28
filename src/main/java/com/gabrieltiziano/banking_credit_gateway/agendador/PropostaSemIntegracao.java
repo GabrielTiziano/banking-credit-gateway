7c59ab6d353ed3a1630c39905f4997efca843f1d
GGhiaroni <gghiaronitiziano@gmail.com>
2026-06-28 11:30:53 -0300

    refactor: Adiciona o método atualizarProposta no agendador para tornar o código mais legível

--- src/main/java/com/gabrieltiziano/banking_credit_gateway/agendador/PropostaSemIntegracao.java
+++ src/main/java/com/gabrieltiziano/banking_credit_gateway/agendador/PropostaSemIntegracao.java
@@ -1,6 +1,7 @@
 package com.gabrieltiziano.banking_credit_gateway.agendador;
 
 import com.gabrieltiziano.banking_credit_gateway.dto.PropostaResponse;
+import com.gabrieltiziano.banking_credit_gateway.entities.Proposta;
 import com.gabrieltiziano.banking_credit_gateway.repository.PropostaRepository;
 import com.gabrieltiziano.banking_credit_gateway.service.NotificacaoService;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,11 +22,15 @@ public class PropostaSemIntegracao {
         propostaRepository.findAllByIntegradaIsFalse().forEach(proposta -> {
             try {
                 notificacaoService.notificar(PropostaResponse.from(proposta), exchange);
-                proposta.setIntegrada(true);
-                propostaRepository.save(proposta);
+                atualizarProposta(proposta);
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         });
+    }
+
+    private void atualizarProposta(Proposta proposta){
+        proposta.setIntegrada(true);
+        propostaRepository.save(proposta);
     }
 }
