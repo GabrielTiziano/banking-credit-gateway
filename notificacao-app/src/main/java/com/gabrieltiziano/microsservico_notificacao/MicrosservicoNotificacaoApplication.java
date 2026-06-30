c8673b1d83860a8577c125f5effeb5d1b39fac30
GGhiaroni <gghiaronitiziano@gmail.com>
2026-06-30 00:11:10 -0300

    feat(notificacao): adiciona estrutura inicial do microsserviço

---
+++ notificacao-app/src/main/java/com/gabrieltiziano/microsservico_notificacao/MicrosservicoNotificacaoApplication.java
@@ -0,0 +1,13 @@
+package com.gabrieltiziano.microsservico_notificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosservicoNotificacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosservicoNotificacaoApplication.class, args);
+	}
+
+}
---
+++ notificacao-app/src/test/java/com/gabrieltiziano/microsservico_notificacao/MicrosservicoNotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrieltiziano.microsservico_notificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosservicoNotificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
