d870b648f24bcb927a7c58de1ac61d565027e0f7
GGhiaroni <gghiaronitiziano@gmail.com>
2026-06-29 23:35:52 -0300

    feat: Adiciona o localstack no docker compose, as credenciais no application.yml, a dependência no pom.xml e configura a AwsConfig e o SmsService

---
+++ src/main/java/com/gabrieltiziano/banking_credit_gateway/config/AwsConfig.java
@@ -0,0 +1,26 @@
+package com.gabrieltiziano.banking_credit_gateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sns.SnsClient;
+
+import java.net.URI;
+
+@Configuration
+public class AwsConfig {
+
+    @Bean
+    public SnsClient snsClient(@Value("${aws.sns.endpoint}") String endpoint,
+                               @Value("${aws.region}") String region) {
+        return SnsClient.builder()
+                .endpointOverride(URI.create(endpoint))
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create("test", "test")))
+                .build();
+    }
+}
