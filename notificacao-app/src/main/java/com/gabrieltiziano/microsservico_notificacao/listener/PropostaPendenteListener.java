81bfb142c80260fb90604cb9a723e9092ac1e749
GGhiaroni <gghiaronitiziano@gmail.com>
2026-07-04 11:05:44 -0300

    feat: Adiciona o listener da fila proposta-pendente

---
+++ notificacao-app/src/main/java/com/gabrieltiziano/microsservico_notificacao/listener/PropostaPendenteListener.java
@@ -0,0 +1,13 @@
+package com.gabrieltiziano.microsservico_notificacao.listener;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PropostaPendenteListener {
+
+    @RabbitListener(queues = "${rabbitmq.queue.proposta.pendente}")
+    public void propostaPendente(){
+
+    }
+}
