226a47174e72fdf3e4d5ae84147c05457e22ef77
GGhiaroni <gghiaronitiziano@gmail.com>
2026-07-04 11:31:57 -0300

    feat: consome a fila e envia SMS via SNS ao receber a proposta

--- notificacao-app/src/main/java/com/gabrieltiziano/microsservico_notificacao/listener/PropostaPendenteListener.java
+++ notificacao-app/src/main/java/com/gabrieltiziano/microsservico_notificacao/listener/PropostaPendenteListener.java
@@ -1,13 +1,27 @@
 package com.gabrieltiziano.microsservico_notificacao.listener;
 
+import com.gabrieltiziano.microsservico_notificacao.constante.MensagemConstante;
+import com.gabrieltiziano.microsservico_notificacao.domain.Proposta;
+import com.gabrieltiziano.microsservico_notificacao.service.SmsService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PropostaPendenteListener {
+    private final SmsService smsService;
+
+    public PropostaPendenteListener(SmsService smsService) {
+        this.smsService = smsService;
+    }
 
     @RabbitListener(queues = "${rabbitmq.queue.proposta.pendente}")
-    public void propostaPendente(){
+    public void propostaPendente(Proposta proposta){
+        String mensagem = String.format(
+                MensagemConstante.PROPOSTA_EM_ANALISE,
+                proposta.getUsuario().getNome(),
+                proposta.getValorSolicitado(),
+                proposta.getPrazoPagamento());
 
+        smsService.enviarSms(proposta.getUsuario().getTelefone(), mensagem);
     }
 }
