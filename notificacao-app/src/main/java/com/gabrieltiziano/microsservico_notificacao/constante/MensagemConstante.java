211899d2cf446505378416bdedc2146b809ffe50
GGhiaroni <gghiaronitiziano@gmail.com>
2026-07-04 11:31:41 -0300

    feat: adiciona template da mensagem de proposta em análise

---
+++ notificacao-app/src/main/java/com/gabrieltiziano/microsservico_notificacao/constante/MensagemConstante.java
@@ -0,0 +1,6 @@
+package com.gabrieltiziano.microsservico_notificacao.constante;
+
+public class MensagemConstante {
+    private MensagemConstante(){}
+    public static final String PROPOSTA_EM_ANALISE = "Prezado(a) %s, a sua proposta de crédito no valor de R$ %.2f, com prazo de pagamento de %d meses, está em análise. Em breve você receberá uma resposta.";
+}
